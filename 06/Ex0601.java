ae2b0b202b490c13cfa2cac151913514fb26b3fa
Nanae-j <nanae9577@gmail.com>
2025-05-04 16:07:22 +0900

    [update]セクション6完了
    データ型
    
    リテラル、基本データ型、String型
    プリミティブ型と参照型

---
+++ 06/Ex0601.java
@@ -0,0 +1,11 @@
+class Ex0601 {
+  public static void main(String[] args) {
+    String name = "速水ごう";
+    int month = 8;
+    int day = 21;
+
+    String greet = name + "です。" + "誕生日は" + month + "月" + day + "日です。";
+
+    System.out.println(greet);
+  }
+}
