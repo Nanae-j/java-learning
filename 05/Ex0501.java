11f1f7bc7ecfa799edc7566cbff13b4c1cc5d52f
Nanae-j <nanae9577@gmail.com>
2025-05-04 15:34:04 +0900

    [update]セクション5完了
    変数の宣言と代入

---
+++ 05/Ex0501.java
@@ -0,0 +1,12 @@
+class Ex0501 {
+  public static void main(String[] args) {
+    int amount;
+    amount = 300;
+
+    int point;
+    point = 9;
+
+    System.out.println("金額:" + amount + "円");
+    System.out.println("獲得したポイント:" + point + "ポイント");
+  }
+}
---
+++ 05/Ex0502.java
@@ -0,0 +1,12 @@
+class Ex0502 {
+  public static void main(String[] args) {
+    final int START_TIME;
+    START_TIME = 9;
+
+    int nowTime;
+    nowTime = 7;
+
+    System.out.println("開始時刻:" + START_TIME + "時");
+    System.out.println("現在時刻:" + nowTime + "時");
+  }
+}
