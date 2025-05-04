a89fe661863a5812b4981a79bf326fec16b4ff9d
Nanae-j <nanae9577@gmail.com>
2025-05-04 14:09:44 +0900

    [update]セクション４終了
    順次でプログラム実行

---
+++ 04/Ex0401.java
@@ -0,0 +1,7 @@
+class Ex0401 {
+  public static void main(String[] args) {
+    System.out.print("こんにちは。");
+    System.out.println("山本です。北海道出身です。");
+    System.out.println("好きな食べ物は焼肉です。");
+  }
+}
